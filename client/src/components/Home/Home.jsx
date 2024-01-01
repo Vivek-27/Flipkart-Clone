5b18b1769518bbda3945350120d8c3e50d59bc48
Vivek-27 <vivekvb@protonmail.com>
2024-01-01 18:38:23 +0530

    slide updated

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -24,25 +24,26 @@ const Home = () => {
     dispatch(getProducts());
   }, [dispatch]);
 
-  const products1 = products.slice(0, 8);
-  const products2 = products.slice(7, 16);
-  console.log(products);
+  const products2 = products.slice(Math.floor(Math.random() * 18), 26);
+  const products1 = products.slice(Math.floor(Math.random() * 10), 17);
+  const products5 = products.slice(Math.floor(Math.random() * 23), 36);
+  const products3 = products.slice(Math.floor(Math.random() * 36), 43);
 
   return (
     <>
       <Navbar />
       <Component>
         <Banner />
-        <MidSlide products={products} title="Deal of the Day" timer={true} />
+        <MidSlide products={products2} title="Deal of the Day" timer={true} />
         <MidSection />
-        <Slide products={products1} title="Discounts for You" timer={false} />
-        <Slide products={products2} title="Suggesting Items" timer={false} />
-        <Slide products={products1} title="Top Selection" timer={false} />
-        <Slide products={products} title="Recommended Items" timer={false} />
-        <Slide products={products2} title="Trending Offers" timer={false} />
-        <Slide products={products} title="Season's top picks" timer={false} />
+        <Slide products={products2} title="Discounts for You" timer={false} />
+        <Slide products={products1} title="Suggesting Items" timer={false} />
+        <Slide products={products3} title="Top Selection" timer={false} />
+        <Slide products={products2} title="Recommended Items" timer={false} />
+        <Slide products={products1} title="Trending Offers" timer={false} />
+        <Slide products={products3} title="Season's top picks" timer={false} />
         <Slide
-          products={products}
+          products={products5}
           title="Top Deals on Accessories"
           timer={false}
         />
